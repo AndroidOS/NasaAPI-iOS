2b90fe66d82ab0f585f0de3fbdf6a710ea48b2e2
AndroidOS <burrowsgit@gmail.com>
2021-02-05 18:20:28 +1100

    Added imageView loading (1)

--- NasaAPI/ViewController.swift
+++ NasaAPI/ViewController.swift
@@ -10,23 +10,45 @@ import UIKit
 class ViewController: UIViewController, NasaDataManagerDelegate {
     
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     var dataManager = NasaDataManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "1024")
         dataManager.fetchNasaData()
+        
     }
     
     func didUpdateNasa(picURLs: [String]) {
         print("didUpdateNasa")
         print(picURLs)
+        let url = URL(string: picURLs[0])!
+        downloadImage(from: url)
     }
     
 
 
     @IBAction func btnDownload(_ sender: Any) {
         dataManager.fetchNasaData()
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func downloadImage(from url: URL) {
+        print("Download Started")
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { [weak self] in
+                self?.imageView.image = UIImage(data: data)
+            }
+        }
     }
 }
 
