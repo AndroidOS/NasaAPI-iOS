c4759e0536275f452548cff60e8cbf5d7397fd2d
AndroidOS <burrowsgit@gmail.com>
2021-03-14 12:15:32 +1100

    Added Weather Table (1)

---
+++ NasaAPI/Controller/WeatherTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  WeatherViewController.swift
+//  NasaAPI
+//
+//  Created by Manuel Carvalho on 1/3/21.
+//
+
+import UIKit
+
+class WeatherTableViewController: UITableViewController, WeatherDataManagerDelegate  {
+    
+    
+    
+    let myList = [1,2,3,4,5]
+    
+    var dataManager = WeatherDataManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataManager.delegate = self
+        dataManager.fetchNasaData(sol: 1000)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        
+        cell.textLabel?.text = "\(myList[indexPath.row])"
+
+        return cell
+    }
+    
+
+    func didUpdateWeather(weatherData: String) {
+        //print("didUpdateWeather \(weatherData)")
+        
+        let mydate = weatherData[weatherData.index(weatherData.startIndex, offsetBy: 3) ..< weatherData.index(weatherData.endIndex, offsetBy: -1)]
+        
+        DispatchQueue.main.async {
+            
+        }
+        
+       
+        
+    }
+    
+    
+
+}
